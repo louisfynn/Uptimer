7f74ed26c7cd7d8752d90ca2a586877e6b1524bb
ItachiU <xavjon17@gmail.com>
2022-10-24 11:37:01 -0500

    x

---
+++ Assets/Config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TOKEN: process.env.TOKEN || "MTAzNDEwOTY1MTY3NzA4NTcwNg.Gmi2lS.9DB9H4mhA2FK0PBBEt3Dep8k3TEaC8DO5bLQ84",
+  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://Excriyin:Zibet5678@uptimer.i94sfje.mongodb.net/?retryWrites=true&w=majority",
+  CLIENT_ID: process.env.CLIENT_ID || "1034109651677085706",
+  OWNERS: ["785682740266926080"],
+  SUPPORT_SERVER: "https://discord.gg/kUMhb37n8b",
+  CHANNELS: {
+    COMMANDS_LOGS: "",
+    ERROR_COMMAND_LOGS: ""
+  }
+}
