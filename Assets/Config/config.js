1375f07f364262cb7704a803189cd17e1247112c
louisfynn <117991000+louisfynn@users.noreply.github.com>
2022-11-11 17:53:25 +0530

    Update config.js

--- Assets/Config/config.js
+++ Assets/Config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  TOKEN: process.env.TOKEN || "",
-  MONGO_DB: process.env.MONGO_DB || "",
-  CLIENT_ID: process.env.CLIENT_ID || "",
-  OWNERS: [""],
-  SUPPORT_SERVER: "",
+  TOKEN: process.env.TOKEN || "MTA0MDU4NTAzODQ0MzU4NTUzNg.Gf9Alo.R4gw2QlUybBgP0fntp5uQdi6pNkBqKyL2yZe9o",
+  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://Ezekiel:mongodb@cluster0.cqg8rid.mongodb.net/?retryWrites=true&w=majority",
+  CLIENT_ID: process.env.CLIENT_ID || "1040585038443585536",
+  OWNERS: ["736073443107143692"],
+  SUPPORT_SERVER: "839100047294988309",
   CHANNELS: {
-    COMMANDS_LOGS: "",
+    COMMANDS_LOGS: "839100047294988312",
     ERROR_COMMAND_LOGS: ""
   }
 }
